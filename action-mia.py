02b75c5dddb0adfebeb8aa9eceffa0d67a1a752f
Edoliver <36020837+EddOliver@users.noreply.github.com>
2019-04-29 00:53:59 -0500

    Create action-mia.py

---
+++ action-mia.py
@@ -0,0 +1,106 @@
+import configparser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+from Utilities import Utilities
+import io
+import sys
+import os
+
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+
+util = Utilities()
+
+class SnipsConfigParser(configparser.ConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.read_file(f)
+            #conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
+      
+def subscribe_intent_hello(hermes, intentMessage):
+    sayMessage = "General Kenobi!"
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+    
+def subscribe_intent_fine(hermes, intentMessage):
+    sayMessage = "Excellent!, by the way I'm the best!"
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+    
+def subscribe_intent_bye(hermes, intentMessage):
+    sayMessage = "Bye then, this is awkward"
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+def subscribe_intent_lastGlu(hermes, intentMessage):
+    sayMessage = "Your Glucose level was: the number you had via MQTT last time milimiters per deciliter"
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+def subscribe_intent_newGlu(hermes, intentMessage):
+    sayMessage = "Allright, tell me your new measurement."
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_continue_session(current_session_id, sayMessage, ["&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:gluData"])
+
+def subscribe_intent_gluData(hermes, intentMessage):
+    sayMessage = "Okay, it is now logged." 
+    # The number will be logged via the command center
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+def subscribe_intent_newHeart(hermes, intentMessage):
+    sayMessage = "Excellent!, put your finger on the device for slot seconds starting NOW!"
+    #The max30100 will send a publish to the command centre with this this info and will be sorted here.
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+def subscribe_intent_lastHeart(hermes, intentMessage):
+    sayMessage = "Your heart rate was the one detected via MQTT beats per minute"
+    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+def subscribe_intent_historicData(hermes, intentMessage):
+    #again glunumber should be provided by an MQTT publish from the command centre, this is just for example purposes
+    glunumber= 100
+    
+    if glunumber > 200:
+        sayMessage = "Staying at a healthy weight can help you prevent and manage problems like prediabetes, type 2 diabetes, heart disease, high blood pressure and unhealthy cholesterol. Keep at it you are doing great. Try not to smoke and do contant exercise!"
+    elif glunumber > 100 and glunumber < 200:
+        sayMessage = "This is not all bad, avoid smoking as much as you can and if possible avoid it, control how much carbohydrates and fats you are eating and try to incorporate a physical activity in your daily routine."
+    elif glunumber < 100:
+        sayMessage = "Staying at a healthy weight can help you prevent and manage problems like prediabetes, type 2 diabetes, heart disease, high blood pressure and unhealthy cholesterol. Keep at it you are doing great. Try not to smoke and do contant exercise!"    
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, sayMessage)
+
+
+
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:hello",       subscribe_intent_hello) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:bye",       subscribe_intent_bye) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:fineYes",         subscribe_intent_fine) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:lastGlu",         subscribe_intent_lastGlu) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:newGlu",         subscribe_intent_newGlu) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:gluData",         subscribe_intent_gluData) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:newHeart",         subscribe_intent_newHeart) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:lastHeart",         subscribe_intent_lastHeart) \
+            .subscribe_intent("&KV4lavkZXDmNqwJXKn6Mxmx6W1jegAM6Oyr2o1PE:historicData",         subscribe_intent_historicData) \
+            .loop_forever()
